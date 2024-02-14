809eb869182cef3c47463d9ab97c7fef2a3a7db1
Jason Dent <Jason3S@users.noreply.github.com>
2024-02-14 13:04:43 +0100

    chore: cleanup update workflow (#12)
    * chore: cleanup update workflow
    * Add update readme workflow

--- samples/histogram.js
+++ samples/histogram.js
@@ -4,6 +4,8 @@ const data = [
     ['one', 1],
     ['two', 2],
     ['three', 3],
+    ['four', 4],
+    ['2.5', 2.5],
 ];
 const hist = histogram(data, { width: 40, maxLabelWidth: 5, title: 'Numbers', headers: ['Label', 'Value'] });
 console.log(hist);
