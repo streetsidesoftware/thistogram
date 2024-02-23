72f1eb55d17e7dd6dc12e2500587c27a2b561931
Jason Dent <Jason3S@users.noreply.github.com>
2024-02-23 08:59:00 +0100

    fix: expose plotPointRelativeToStandardDeviation (#41)

--- src/bars.test.ts
+++ src/bars.test.ts
@@ -5,17 +5,18 @@ import { barHorizontal, plotPointRelativeToStandardDeviation, simpleHistogram } 
 describe('simpleHistogram', () => {
     test('should return a string of the histogram', () => {
         const data = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        const result = simpleHistogram(data);
+        const result = simpleHistogram(data, 0, 9);
         expect(result).toBe(' ▁▂▃▄▅▆▇█');
     });
 
     test.each`
         data                                   | min          | max          | expected
-        ${[0, 1, 2, 3, 4, 5, 6, 7, 8]}         | ${undefined} | ${undefined} | ${' ▁▂▃▄▅▆▇█'}
+        ${[0, 1, 2, 3, 4, 5, 6, 7, 8]}         | ${undefined} | ${undefined} | ${'▁▂▃▄▄▅▆▇█'}
+        ${[0, 0, 0]}                           | ${undefined} | ${undefined} | ${'   '}
         ${[1, 2, 3, 4, 5, 6, 7, 8]}            | ${0}         | ${undefined} | ${'▁▂▃▄▅▆▇█'}
         ${[1, 2, 3, 4, 5, 6, 7, 8]}            | ${4}         | ${undefined} | ${'    ▂▄▆█'}
         ${[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]} | ${0}         | ${undefined} | ${'   ▁▂▃▄▅▆▇█'}
-        ${[0, 1, 2, 3, 4, 5, 1, 7, 8, 9, 10]}  | ${undefined} | ${8}         | ${' ▁▂▃▄▅▁▇███'}
+        ${[0, 1, 2, 3, 4, 5, 1, 7, 8, 9, 10]}  | ${undefined} | ${8}         | ${'▁▂▃▄▅▆▂████'}
     `('simpleHistogram $data', ({ data, min, max, expected }) => {
         const result = simpleHistogram(data, min, max);
         expect(result).toBe(expected);
@@ -26,7 +27,7 @@ describe('simpleHistogram', () => {
         const barChars = ['-', '+', '*', '#'];
         const result = simpleHistogram(data, undefined, undefined, barChars);
         expect(result.length).toBe(data.length);
-        expect(result).toBe(' -+*#');
+        expect(result).toBe('-++*#');
     });
 });
 
@@ -58,12 +59,13 @@ describe('plotPointRelativeToStandardDeviation', () => {
     });
 
     test.each`
-        point  | sd   | mean | width | range | expected
-        ${-1}  | ${2} | ${0} | ${25} | ${2}  | ${'┣━━━━━┻━━●━━╋━━━━━┻━━━━━┫'}
-        ${1}   | ${2} | ${0} | ${35} | ${3}  | ${'     ┣━━━━━┻━━━━━╋━━●━━┻━━━━━┫     '}
-        ${5}   | ${2} | ${0} | ${35} | ${3}  | ${'     ┣━━━━━┻━━━━━╋━━━━━┻━━━━━┫  ●  '}
-        ${-10} | ${2} | ${0} | ${35} | ${3}  | ${'●    ┣━━━━━┻━━━━━╋━━━━━┻━━━━━┫     '}
-        ${1}   | ${2} | ${0} | ${34} | ${3}  | ${'      ┣━━━━┻━━━━━╋━━●━━┻━━━━┫     '}
+        point  | sd   | mean | width | range        | expected
+        ${-1}  | ${2} | ${0} | ${25} | ${2}         | ${'┣━━━━━┻━━●━━╋━━━━━┻━━━━━┫'}
+        ${-1}  | ${2} | ${0} | ${25} | ${undefined} | ${'  ┣━━━━┻━━●━╋━━━━┻━━━━┫  '}
+        ${1}   | ${2} | ${0} | ${35} | ${3}         | ${'     ┣━━━━━┻━━━━━╋━━●━━┻━━━━━┫     '}
+        ${5}   | ${2} | ${0} | ${35} | ${3}         | ${'     ┣━━━━━┻━━━━━╋━━━━━┻━━━━━┫  ●  '}
+        ${-10} | ${2} | ${0} | ${35} | ${3}         | ${'●    ┣━━━━━┻━━━━━╋━━━━━┻━━━━━┫     '}
+        ${1}   | ${2} | ${0} | ${34} | ${3}         | ${'      ┣━━━━┻━━━━━╋━━●━━┻━━━━┫     '}
     `('plotPointRelativeToStandardDeviation $point', ({ point, sd, mean, width, range, expected }) => {
         const result = plotPointRelativeToStandardDeviation(point, sd, mean, width, range);
         expect(result).toBe(expected);
